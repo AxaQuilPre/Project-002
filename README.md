036eb43835d5978ddf65c832613fffeb4f8799e1
AxaQuilPre <112755557+AxaQuilPre@users.noreply.github.com>
2022-10-06 17:08:50 +0200

    Working

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project-002
+## For BLACKOUT PMC, financial agents.
+*An auomated receipts creator to quickly log deployments & operations*
--- main_file.py
+++ main_file.py
@@ -137,14 +137,77 @@ def hardcore_calculator():
         final_draft = final_draft + f"\n \n **Total: -${number_divider(total)}**"
     return final_draft
 
-
+def divisional_calculator():
+    final_draft = "**Contracted Patrol by (name)** \n Link: \n \n **Bonuses**"
+    question = int(input("Did the event take over 40 minutes? \n Choices: \n Type 1 for yes \n Type 2 for no \n"))
+    if question == 1:
+        total = 800000
+        final_draft = final_draft + "\n Minimum Payment: $800'000"
+    elif question == 2:
+        total = 400000
+        final_draft = final_draft + "\n Minimum Payment: $400'000"
+    question = int(input("Were at lest 4 settlement cleared? \n Choices: \n Type 1 for yes \n Type 2 for no \n"))
+    if question == 1:
+        total += 100000
+        final_draft = final_draft + "\n 4+ Settlements cleared: $100'000"
+    question = int(input("How many main bases were cleared? \n Type the number below \n"))
+    total += question * 250000
+    final_draft = final_draft + f"\n {question}x Main Bases cleared: $" + number_divider(question * 250000)
+    question = int(input("Was 1 wave held in at least 5 locations? \n Choices: \n Type 1 for yes \n Type 2 for no \n"))
+    if question == 1:
+        total += 200000
+        final_draft = final_draft + "\n Waves held: $200'000"
+    question = int(input("How many main bases were held for 2+ waves? \n"))
+    if question == 1 :
+        total += question * 500000
+        final_draft = final_draft + f"{question}x Main Bases held for 2+ waves: ${number_divider(question * 500000)} \n "
+    final_draft = final_draft + f"\n \n *Sub-Total: $" + number_divider(total) + "*"
+    # All penalities are calculated below from here on
+    final_draft = final_draft + " \n \n **KIA Penalities**"
+    question = int(input("How many KIA's? \n"))
+    total -= question * 75000
+    final_draft = final_draft + f"\n {question}x KIA's: -$" + number_divider(question * 75000)
+    if question == 0:
+        total += 200000
+        final_draft = final_draft + "\n 0 KIA's Bonus: $200'000"
+    elif question >= 4 and question <= 7:
+       total -= 300000
+       final_draft = final_draft + "\n 4-7 KIA's: -$300'000"
+    elif question >= 8 and question <= 11:
+        total -= 500000
+        final_draft = final_draft + "\n 8-11 KIA's: -$500'000"
+    elif question > 12:
+        total -= 700000
+        final_draft = final_draft + "\n 12+ KIA's: -$700'000"
+    question = int(input("How many VDIA's? \n"))
+    if question == 0:
+        total += 150000
+        final_draft = final_draft + "\n 0 VDIA's Bonus: $150'000"
+    elif question >= 1 and question <= 2:
+        total -= 200000
+        final_draft = final_draft + "\n 1-2 VDIA's: -$200'000"
+    elif question >= 3 and question <= 5:
+        total -= 400000
+        final_draft = final_draft + "\n 3-5 VDIA's: -$400'000"
+    elif question >= 6:
+        total -= 600000
+        final_draft = final_draft + "\n 6+ VDIA's: -$600'000"
+#Totals and final formatting
+    if total >= 0:
+        final_draft = final_draft + f"\n \n **Total: ${number_divider(total)}**"
+    else:
+        total = total * -1
+        final_draft = final_draft + f"\n \n **Total: -${number_divider(total)}**"
+    return final_draft
 
 #User callings
-query = int(input(" Type 1 or 2 to select \n 1: Patrol \n 2: Hardcore Patrol \n"))
+query = int(input(" Type 1 or 2 to select \n 1: Patrol \n 2: Hardcore Patrol \n 3: Divisional Patrol \n"))
 if query == 1:
     print(patrol_calculator())
 elif query == 2:
     print(hardcore_calculator())
+elif query == 3:
+    print(divisional_calculator())
 
 
 
